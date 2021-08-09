2991360fb675664ab4168ede370e5afff0bc16ee
habtamuabera <88686079+habtamuabera@users.noreply.github.com>
2021-08-09 23:37:11 +0100

    Python data habtamu (#1)
    * iam commiting hello world
    
    * iam commiting hello world
    
    * iam commiting hello world2

---
+++ test_python/main.py
@@ -0,0 +1,7 @@
+
+print("hello world")
+# git add .
+# git commit -m "Commit message"
+# git push origin main
+ git config --global user.email "s218478@studenti.polito.it"
+  git config --global user.name "habtamu"
